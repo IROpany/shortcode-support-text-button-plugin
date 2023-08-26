5526a2fff9bbbe57c3521c46ee384ff513dba830
IRO <56658124+IROpany@users.noreply.github.com>
2023-08-26 21:20:13 +0900

    Add files via upload

---
+++ custom-support-button-plugin/custom-support-button-plugin.php
@@ -0,0 +1,36 @@
+<?php
+/*
+Plugin Name: Custom Support Button
+Description: Add a custom support button to your posts.
+Version: 1.0
+Author: Your Name
+*/
+
+function custom_support_button($atts) {
+    $atts = shortcode_atts(
+        array(
+            'count' => true,
+        ),
+        $atts
+    );
+
+    $output = '<div class="custom-support-button">';
+    $output .= '<button class="support-button">応援してるよ</button>';
+    if ($atts['count']) {
+        $output .= '<span class="support-count">0</span>';
+    }
+    $output .= '<span class="thank-you-message"></span>';
+    $output .= '</div>';
+
+    return $output;
+}
+add_shortcode('support_button', 'custom_support_button');
+
+function custom_support_button_enqueue_scripts() {
+    wp_enqueue_style('custom-support-button-style', plugin_dir_url(__FILE__) . 'assets/style.css');
+    wp_enqueue_script('custom-support-button-script', plugin_dir_url(__FILE__) . 'assets/script.js', array('jquery'), '1.0', true);
+    wp_localize_script('custom-support-button-script', 'custom_support_button_vars', array(
+        'thank_you_message' => '応援ありがとうございます！励みになります！'
+    ));
+}
+add_action('wp_enqueue_scripts', 'custom_support_button_enqueue_scripts');
